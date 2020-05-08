77a0e8ea098f83246e658ed92224309d57d0db06
rassoll <rassoll32@gmail.com>
2020-05-08 22:49:55 +0300

    XML Configuration added

--- src/main/java/com/github/rakhmedovrs/springbootjokesapp/config/ChuckConfiguration.java
+++ src/main/java/com/github/rakhmedovrs/springbootjokesapp/config/ChuckConfiguration.java
@@ -1,17 +1,15 @@
 package com.github.rakhmedovrs.springbootjokesapp.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author RakhmedovRS
  * @created 08-May-20
  */
-@Configuration
+//@Configuration
 public class ChuckConfiguration
 {
-	@Bean
+	//@Bean
 	public ChuckNorrisQuotes chuckNorrisQuotes()
 	{
 		return new ChuckNorrisQuotes();
